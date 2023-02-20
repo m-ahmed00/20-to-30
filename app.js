cd05428f3b17b0d064c04cd77c4e34581b500e50
muhammad ahmed <114142945+m-ahmed00@users.noreply.github.com>
2023-02-20 19:22:55 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,158 @@
+
+// Question no 3
+// var name = "india"
+// var Num = name.indexOf("n")
+// document.write("Index of n is:" + indexNum)
+
+
+// Question no 4
+// var namee = "ahmed"
+// var indexNum = namee.lastIndexOf("l")
+// document.write("Last Index of l is: " + indexNum)
+// Question no 1
+// var firstName = prompt("Enter Your First Name")
+// var lastName = prompt("Enter Your Last Name")
+// var fullName = firstName + " " + lastName
+// document.write("Hello" + fullName)
+
+
+
+// Question no 5
+// var namee = "Pakistani"
+// var indexNum = name[3]
+// document.write("3rd Index is: " + indexNum)
+// Question no 2
+// var userInp = prompt("Enter Mobile Model")
+// var inpLenght = userInp.length
+// document.write("Lenght of String is: " + inpLenght)
+
+
+
+// Question no 6
+// var firstName = prompt("Enter Your First Name")
+// var lastName = prompt("Enter Your Last Name")
+// var total = firstName.concat(lastName)
+// document.write(total)
+
+
+// Question no 7
+// var city = "Hyderabad"
+// var change = city.slice(5)
+// document.write("Islam"+change)
+
+
+// Question no 8
+// var msg = "Ahmed and ezan are best friends.They play cricket and football together."
+// var change = msg.replace(/and/g,"&")
+// document.write("Before: "+msg+ "<br>"+ "<br>")
+// document.write("After: "+change)
+
+
+// Question no 9
+// var str = "453"
+// document.write(str.toString()+"<br>")
+// document.write(typeof str+"<br>")
+// document.write(str+"<br>")
+// document.write(typeof parseInt(str))
+
+
+// Question no 10
+// var web = prompt("Enter Domain Name in www.abc.com format")
+// var domain = web.slice(4)
+// document.write(domain)
+
+
+// Question no 11
+// var inp = prompt("Enter Something")
+// var change = inp.toUpperCase()
+// document.write("User Input: " + inp+"<br>")
+// document.write("Updated Version: " + change)
+
+
+// Question no 12
+// var inp = prompt("Enter Something")
+// var change = inp.toLowerCase()
+// document.write("User Input: " + inp+"<br>")
+// document.write("Updated Version: " + change)
+
+
+// Question no 13
+// var inp = prompt("Enter Something")
+// var change = inp.slice(0, 1)
+// var change2 = inp.slice(1)
+// var a = change.toUpperCase()
+// var b = change2.toLowerCase()
+// document.write(a + b)
+
+
+// Question no 14
+// var num = 39.52
+// var numToString = num.toString()
+// var str = numToString.slice(0, 2)
+// var str2 = numToString.slice(3)
+// var res = str + str2
+// document.write(res+ "<br>")
+// document.write(typeof res)
+
+
+// Question no 1
+// var inp = 45
+// var round = Math.round(inp)
+// document.write(round + "<br>")
+// document.write(Math.ceil(inp) + "<br>")
+// document.write(Math.floor(inp))
+
+
+// Question no 2
+// var inp = -45
+// var round = Math.round(inp)
+// document.write(round + "<br>")
+// document.write(Math.ceil(inp) + "<br>")
+// document.write(Math.floor(inp))
+
+
+// Question no 3
+// var ahmed = 45.45435
+// var round = Math.round(inp)
+// document.write(round + "<br>")
+// document.write(Math.ceil(inp) + "<br>")
+// document.write(Math.floor(inp))
+
+
+// Question no 4
+// var inp = -45.45435
+// var round = Math.round(inp)
+// document.write(round + "<br>")
+// document.write(Math.ceil(inp) + "<br>")
+// document.write(Math.floor(inp))
+
+
+
+// Question no 5
+// var inp = +prompt("Enter Number")
+// var minus = inp*-1
+// document.write(inp+" and"+minus)
+
+
+
+// Question no 6
+// var rand = Math.random()*7
+// var change = Math.floor(rand)
+// document.write(change)
+
+
+
+// Question no 7
+// var userInp = prompt("Enter 0/1")
+// var rand = Math.random() * 2
+// var change = Math.floor(rand)
+// if (userInp == change) {
+//     document.write("ahmed wins")
+// } else {
+//     document.write("Opponent wins")
+
+// }
+// console.log(change)
+
+
+
